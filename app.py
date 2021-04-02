c2c7ed840043b6a6c91e260eecc2b34be0df19ad
haleyseo <seyoung_seo@brown.edu>
2021-04-02 11:28:04 -0400

    exploring user input into python, ~1 hour

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ def hello_world():
     return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
+
+name = input("Enter your name:")
+print("Hello", name + "!")
