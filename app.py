a0b519dba0cc49b8ce5f085565f1da04fff03b9b
Shirley <shirley_dong@brown.edu>
2021-04-02 11:04:07 -0700

    created naive search for song titles. Shirley - driver, Haley - observer. 1 hour

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+#import csv
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,5 +13,24 @@ def hello_world():
 # if __name__ == '__main__':
 #     app.run()
 
-name = input("Enter your name:")
-print("Hello", name + "!")
+#name = input("Enter your name:")
+#print("Hello", name + "!")
+
+
+# my_data = open('charts.csv','r')
+# charts = my_data.read()
+#
+# print(charts)
+
+
+
+import csv
+
+song_input = input('Enter a song name: ')
+
+with open('charts.csv', 'r') as file:
+    charts = csv.reader(file)
+    for row in charts:
+        if song_input == row[2]:
+            print('On ' + row[0] + ' ' + song_input + ' ranked at #' + row[1] + ' on the Billboard Hot 100 Chart.')
+
