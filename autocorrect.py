4dd0206b150d0281bdc68cbd0d8804c4e2857f1e
Shirley <shirley_dong@brown.edu>
2021-04-12 19:55:54 -0700

    continuing to work on normalize_search function in autocorrect.py. Shirley & Haley debugging. ~1 hr

--- autocorrect.py
+++ autocorrect.py
@@ -63,10 +63,10 @@ import unicodedata2
 
 nlp = spacy.load('en_core_web_sm')
 #nlp_vec = spacy.load('en_vecs', parse = True, tag=True, #entity=True)
-tokenizer = ToktokTokenizer()
-stopword_list = nltk.corpus.stopwords.words('english')
-stopword_list.remove('no')
-stopword_list.remove('not')
+# tokenizer = ToktokTokenizer()
+# stopword_list = nltk.corpus.stopwords.words('english')
+# stopword_list.remove('no')
+# stopword_list.remove('not')
 
 # removing html tags ##
 def strip_html_tags(text):
@@ -78,29 +78,29 @@ def strip_html_tags(text):
 
 ## removing accented characters ##
 
-def remove_accented_chars(text):
-    text = unicodedata2.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-    return text
+# def remove_accented_chars(text):
+#     text = unicodedata2.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+#     return text
 
 # print(remove_accented_chars('Sómě Áccěntěd těxt'))
 
 ## expanding out contractions ##
-def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
-    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
-                                      flags=re.IGNORECASE | re.DOTALL)
-
-    def expand_match(contraction):
-        match = contraction.group(0)
-        first_char = match[0]
-        expanded_contraction = contraction_mapping.get(match) \
-            if contraction_mapping.get(match) \
-            else contraction_mapping.get(match.lower())
-        expanded_contraction = first_char + expanded_contraction[1:]
-        return expanded_contraction
-
-    expanded_text = contractions_pattern.sub(expand_match, text)
-    expanded_text = re.sub("'", "", expanded_text)
-    return expanded_text
+# def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
+#     contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
+#                                       flags=re.IGNORECASE | re.DOTALL)
+#
+#     def expand_match(contraction):
+#         match = contraction.group(0)
+#         first_char = match[0]
+#         expanded_contraction = contraction_mapping.get(match) \
+#             if contraction_mapping.get(match) \
+#             else contraction_mapping.get(match.lower())
+#         expanded_contraction = first_char + expanded_contraction[1:]
+#         return expanded_contraction
+#
+#     expanded_text = contractions_pattern.sub(expand_match, text)
+#     expanded_text = re.sub("'", "", expanded_text)
+#     return expanded_text
 
 
 # print(expand_contractions("Y'all can't expand contractions I'd think"))
@@ -124,69 +124,116 @@ def simple_stemmer(text):
 # print(simple_stemmer("My system keeps crashing his crashed yesterday, ours crashes daily"))
 
 ## lemmatization ##
-
-def lemmatize_text(text):
-    text = nlp(text)
-    text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
-    return text
+#
+# def lemmatize_text(text):
+#     text = nlp(text)
+#     text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
+#     return text
 
 #print(lemmatize_text("My system keeps crashing! his crashed yesterday, ours crashes daily"))
 
 ## removing stopwords ##
 
-def remove_stopwords(text, is_lower_case=False):
-    tokens = tokenizer.tokenize(text)
-    tokens = [token.strip() for token in tokens]
-    if is_lower_case:
-        filtered_tokens = [token for token in tokens if token not in stopword_list]
-    else:
-        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
-    filtered_text = ' '.join(filtered_tokens)
-    return filtered_text
+# def remove_stopwords(text, is_lower_case=False):
+#     tokens = tokenizer.tokenize(text)
+#     tokens = [token.strip() for token in tokens]
+#     if is_lower_case:
+#         filtered_tokens = [token for token in tokens if token not in stopword_list]
+#     else:
+#         filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+#     filtered_text = ' '.join(filtered_tokens)
+#     return filtered_text
 
 # print(remove_stopwords("The, and, if are stopwords, computer is not"))
+
+import re
+from collections import Counter
+
+def words(text): return re.findall(r'\w+', text.lower())
+
+WORDS = Counter(words(open('big.txt').read()))
+
+def P(word, N=sum(WORDS.values())):
+    "Probability of `word`."
+    return WORDS[word] / N
+
+def correction(word):
+    "Most probable spelling correction for word."
+    return max(candidates(word), key=P)
+
+def candidates(word):
+    "Generate possible spelling corrections for word."
+    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+
+def known(words):
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edits2(word):
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
 
 ## all together ##
 def normalize_search(search, html_stripping=True, contraction_expansion=True,
                      accented_char_removal=True, text_lower_case=True,
                      text_lemmatization=True, special_char_removal=True,
-                     stopword_removal=True, remove_digits=True):
+                     stopword_removal=True, remove_digits=True, fixed=True):
     search_input = search.split()
+    print(search_input)
     normalized_search = []
     # normalize each document in the corpus
     for word in search_input:
         # strip HTML
         if html_stripping:
             word = strip_html_tags(word)
+            print(word)
         # remove accented characters
-        if accented_char_removal:
-            word = remove_accented_chars(word)
+        # if accented_char_removal:
+        #     word = remove_accented_chars(word)
         # expand contractions
-        if contraction_expansion:
-            word = expand_contractions(word)
+        # if contraction_expansion:
+        #     word = expand_contractions(word)
         # lowercase the text
         if text_lower_case:
             word = word.lower()
+            print(word)
         # remove extra newlines
         word = re.sub(r'[\r|\n|\r\n]+', ' ', word)
         # lemmatize text
-        if text_lemmatization:
-            word = lemmatize_text(word)
+        # if text_lemmatization:
+        #     word = lemmatize_text(word)
         # remove special characters and\or digits
         if special_char_removal:
             # insert spaces between special characters to isolate them
             special_char_pattern = re.compile(r'([{.(-)!}])')
             word = special_char_pattern.sub(" \\1 ", word)
             word = remove_special_characters(word, remove_digits=remove_digits)
+            print(word)
             # remove extra whitespace
         word = re.sub(' +', ' ', word)
         # remove stopwords
-        if stopword_removal:
-            word = remove_stopwords(word, is_lower_case=text_lower_case)
+        # if stopword_removal:
+        #     word = remove_stopwords(word, is_lower_case=text_lower_case)
+        if fixed:
+            word = correction(word)
+            print(word)
+        search_output = normalized_search.append(str(word))
+    print(search_output)
+    print(type(search_output))
+    list_to_str = ''.join([str(item) for item in search_output])
 
-        normalized_search.append(word)
-
-    return normalized_search
+    return list_to_str
 
 
 # ## TEST EXAMPLE CASE WITH NEWS ARTICLES ##
